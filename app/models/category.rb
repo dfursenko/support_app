8e5eb8454a8d4aa5be42c815b47456887bb181e6
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 09:12:39 +0700

    add taxonomy parent-children to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  belongs_to :parent
+  belongs_to :parent, class_name: 'Category', optional: true
+  has_many :children, class_name: 'Category', foreign_key: 'parent_id'
 end
