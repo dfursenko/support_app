455a3e0d1411208463ab5401114863881bf033be
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 13:29:31 +0700

    edit CreateTickets migration

--- db/migrate/20181007055241_create_tickets.rb
+++ db/migrate/20181007055241_create_tickets.rb
@@ -1,10 +1,10 @@
 class CreateTickets < ActiveRecord::Migration[5.1]
   def change
     create_table :tickets do |t|
-      t.references :user, foreign_key: true
-      t.text :body
+      t.references :user, null: false, foreign_key: true
+      t.text :body, null: false
       t.references :moderator, foreign_key: true
-      t.boolean :status
+      t.boolean :status, null: false, default: true
 
       t.timestamps
     end
