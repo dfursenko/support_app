2a222909ad82030fb9ea12e6c286725b40325628
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 09:31:38 +0700

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180924091230) do
+ActiveRecord::Schema.define(version: 20181007020343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.integer "level"
+    t.bigint "parent_id"
+    t.boolean "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
