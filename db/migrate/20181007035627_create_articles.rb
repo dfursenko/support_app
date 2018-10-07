3b5145ecf2fafdc9f662747dbb6a442c569753f2
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 11:01:29 +0700

    generate scaffold Article user:references category:references title:string body:text published:boolean

---
+++ db/migrate/20181007035627_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :articles do |t|
+      t.references :user, foreign_key: true
+      t.references :category, foreign_key: true
+      t.string :title
+      t.text :body
+      t.boolean :published
+
+      t.timestamps
+    end
+  end
+end
