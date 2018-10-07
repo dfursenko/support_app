661d7cb52ae92e17e119d649aee814d33296576e
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 15:28:54 +0700

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181007071514) do
+ActiveRecord::Schema.define(version: 20181007073707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,12 +38,13 @@ ActiveRecord::Schema.define(version: 20181007071514) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.string "commentable_type"
     t.bigint "commentable_id"
     t.string "body"
     t.boolean "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["commentable_id"], name: "index_comments_on_commentable_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
   create_table "roles", force: :cascade do |t|
