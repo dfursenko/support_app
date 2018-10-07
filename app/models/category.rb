047d7cce64c28a7bc3d9d16e1922dfe4a1314729
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 10:33:48 +0700

    add validates to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :parent, class_name: 'Category', optional: true
   has_many :children, class_name: 'Category', foreign_key: 'parent_id'
+
+  validates :title,     presence: true,   length:       { in: 2..50 }
+  validates :level,     presence: true,   numericality: { only_integer: true }
+  validates :parent,    presence: true
+  validates :published, inclusion: { in: [true, false] }
 end
