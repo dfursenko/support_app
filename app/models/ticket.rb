432cf1d3993b1b231bfd83eeaaa6b4830f7d4618
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 13:58:11 +0700

    add validates to Ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,9 @@
 class Ticket < ApplicationRecord
   belongs_to :user,       class_name: 'User', inverse_of: 'requests'
   belongs_to :moderator,  class_name: 'User', inverse_of: 'tickets'
+
+  validates :user,      presence: true
+  validates :body,      presence: true
+  validates :moderator, presence: true
+  validates :status,    inclusion: { in: [true, false] }
 end
