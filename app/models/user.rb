58046cffdd9dff157cce303979f2aee0dd3e80f7
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 22:45:08 +0700

    add polymorphic of comment to controllers and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :articles
   has_many :requests, class_name: 'Ticket', foreign_key: 'user_id'
   has_many :tickets,  class_name: 'Ticket', foreign_key: 'moderator_id'
+  has_many :comments
 
   validates :email,     presence: true, uniqueness: true
   validates :password,  presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :tickets
   resources :articles
   resources :categories
+  resources :articles do
+    resources :comments, module: :articles
+  end
+  resources :tickets do
+    resources :comments, module: :tickets
+  end
   root 'page#index'
   get 'page/about'
 end
