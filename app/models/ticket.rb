0db86256496d0babf16f23f0c3746134ba652a28
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 13:30:38 +0700

    add users-tickets relations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,4 @@
 class Ticket < ApplicationRecord
-  belongs_to :user
-  belongs_to :moderator
+  belongs_to :user,       class_name: 'User', inverse_of: 'requests'
+  belongs_to :moderator,  class_name: 'User', inverse_of: 'tickets'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   belongs_to :role
   has_many :articles
+  has_many :requests, class_name: 'Ticket', foreign_key: 'user_id'
+  has_many :tickets,  class_name: 'Ticket', foreign_key: 'moderator_id'
 end
