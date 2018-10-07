656ff5537ca5a1a15de53a93a7f41220ef3a8d02
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 11:16:04 +0700

    add validates to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :user,      presence: true
+  validates :category,  presence: true
+  validates :title,     presence: true, length: { in: 2..150 }
+  validates :published, inclusion: { in: [true, false] }
 end
