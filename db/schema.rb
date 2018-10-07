c6bd35f62fa359f1827762c557479b86a8942c12
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 11:12:23 +0700

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181007020343) do
+ActiveRecord::Schema.define(version: 20181007035627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "category_id"
+    t.string "title", null: false
+    t.text "body"
+    t.boolean "published", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -42,5 +54,7 @@ ActiveRecord::Schema.define(version: 20181007020343) do
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "articles", "categories"
+  add_foreign_key "articles", "users"
   add_foreign_key "users", "roles"
 end
