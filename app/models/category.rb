7bdfd07aaee25fddc900b1e9c0debe56050bf4d1
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-07 11:02:23 +0700

    add article relations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :parent, class_name: 'Category', optional: true
   has_many :children, class_name: 'Category', foreign_key: 'parent_id'
+  has_many :articles
 
   validates :title,     presence: true,   length:       { in: 2..50 }
   validates :level,     presence: true,   numericality: { only_integer: true }
--- db/migrate/20181007035627_create_articles.rb
+++ db/migrate/20181007035627_create_articles.rb
@@ -3,9 +3,9 @@ class CreateArticles < ActiveRecord::Migration[5.1]
     create_table :articles do |t|
       t.references :user, foreign_key: true
       t.references :category, foreign_key: true
-      t.string :title
+      t.string :title, null: false
       t.text :body
-      t.boolean :published
+      t.boolean :published, default: false
 
       t.timestamps
     end
