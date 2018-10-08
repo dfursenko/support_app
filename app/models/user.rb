c3f983aa103fb730dbd35138d35d415fb72961b1
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-08 18:19:39 +0700

    generate devise:views and generate devise User (needs fixing)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   belongs_to :role
   has_many :articles, dependent: :destroy
   has_many :requests, class_name: 'Ticket', foreign_key: 'user_id', dependent: :destroy
@@ -8,5 +12,5 @@ class User < ApplicationRecord
 
   validates :email,     presence: true, uniqueness: true
   validates :password,  presence: true
-  validates :role,      presence: true
+  # validates :role,      presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   resources :marks
   resources :tags
   resources :comments
