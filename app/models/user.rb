ab4e8fd64f40d57d82d1fde761de93906814392d
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-15 01:21:36 +0700

    change user for rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :role
+  # has_and_belongs_to_many :roles, join_table: :users_roles
   has_many :articles, dependent: :destroy
   has_many :requests, class_name: 'Ticket', foreign_key: 'user_id', dependent: :destroy
   has_many :tickets,  class_name: 'Ticket', foreign_key: 'moderator_id', dependent: :destroy
