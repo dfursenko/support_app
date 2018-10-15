f088b18df99bdc32f07aa05c6f7b96ac9f5ed44f
Dmitry Fursenko <dfursenko@gmail.com>
2018-10-15 21:59:10 +0700

    change user root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    current_user
+    # current_user
+    tickets_path
   end
 
   rescue_from CanCan::AccessDenied do |exception|
